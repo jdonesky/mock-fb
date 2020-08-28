c750dee2e9c705b613ea166928828df04ed1bef2
Jon <jdonesky@gmail.com>
2020-08-28 07:38:28 -0400

    added modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,1 +1,15 @@
+import React from 'react' 
+import Backdrop from '../Backdrop/Backdrop'
 
+const modal = (props) => {
+  return (
+    <React.Fragment>
+      <div className={classes.Modal} show={props.show} close={props.close}>
+        {props.children}
+      </div>
+      <Backdrop show={props.show} close={props.close} />
+    </React.Fragment>
+  );
+};
+
+export default modal
