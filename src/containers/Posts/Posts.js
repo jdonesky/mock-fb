3e31b7aa7b6f22ebda5b2c9cb19ccc1be58574ee
Jon <jdonesky@gmail.com>
2020-09-01 23:00:03 -0400

    fixed issue with double request in Posts componentDidMount hook

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -9,9 +9,11 @@ import * as actions from "../../store/actions/index";
 
 class Posts extends Component {
   componentDidMount() {
-    this.props.onFetchPosts("posts");
+   
     if (this.props.isAuthenticated) {
       this.props.onFetchPosts("posts", this.props.userId);
+    } else {
+      this.props.onFetchPosts("posts");
     }
   }
 
