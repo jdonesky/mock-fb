6dce04435a325c30e39918e8e451592a6bc1c5f9
Jon <jdonesky@gmail.com>
2020-11-22 14:34:20 -0500

    started post emoji selector

---
+++ src/components/Profile/Timeline/Posts/Post/Dropdowns/Emojis/EmojiSelector.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import classes from './EmojiSelector.css'
+
+const emojiSelector = (props) => {
+
+    const emojis = [
+        {url: , id:1},
+        {url: , id:2},
+        {url: , id:3},
+        {url: , id:4},
+        {url: , id:5},
+        {url: , id:6},
+        {url: , id:7},
+    ]
+
+    return (
+        <div className={classes.EmojiSelectorContainer}>
+
+        </div>
+    );
+}
