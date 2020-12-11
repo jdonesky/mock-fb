bb17c659d3a3f20a9427e7b505a932e70873f582
Jon <jdonesky@gmail.com>
2020-12-10 21:59:23 -0500

    adding contents to profileSummary dropdown - about entries

--- src/components/Users/Dropdowns/ProfileSummary/ProfileSummary.js
+++ src/components/Users/Dropdowns/ProfileSummary/ProfileSummary.js
@@ -4,6 +4,12 @@ import classes from './ProfileSummary.css';
 import * as actions from '../../../../store/actions/index';
 
 import Avatar from '../../../../assets/images/profile-placeholder-gender-neutral';
+import Friends from '../../../../assets/images/UserActivityIcons/friends';
+import Link from '../../../../assets/images/UserActivityIcons/link';
+import Home from '../../../../assets/images/UserActivityIcons/home';
+import Pin from '../../../../assets/images/UserActivityIcons/pin';
+import GradCap from '../../../../assets/images/UserActivityIcons/gradCap';
+import BriefCase from '../../../../assets/images/UserActivityIcons/briefcase';
 
 const profileSummaryDropdown = (props) => {
 
@@ -14,16 +20,41 @@ const profileSummaryDropdown = (props) => {
     }, [onFetchPublicProfile, publicProfileKey, authToken])
 
     let mutualFriends;
-    if (props.friends && props.friends.length && props.myFriends && props.myFriends.length) {
+    if (props.profile.friends && props.profile.friends.length && props.myFriends && props.myFriends.length) {
         mutualFriends = props.myFriends.filter(myFriend => props.friends.map(theirFriend => theirFriend.userId).includes(myFriend.userId))
     }
 
     let firstInfo;
+    let secondInfo;
+    let firstInfoIcon;
+    let secondInfoIcon;
+
     if (mutualFriends) {
-        firstInfo = mutualFriends
+        if (mutualFriends.length === 1) {
+            firstInfo = <span className={classes.InfoEntry}>{"1 mutual friend:"} <b className={classes.MutualFriend}>{mutualFriends[0]}</b></span>
+        } else if (mutualFriends.length === 2) {
+            firstInfo = <span className={classes.InfoEntry}>{"2 mutual friends, including"} <b className={classes.MutualFriend}>{mutualFriends[0]}</b></span>
+        } else {
+            firstInfo = <span className={classes.InfoEntry}>{`${mutualFriends.length} mutual friends, including`} <b className={classes.MutualFriend}>{mutualFriends[0]}</b> and <b className={classes.MutualFriend}>{mutualFriends[1]}</b></span>
+        }
+        firstInfoIcon = <Friends />
+    } else {
+        if (props.profile.friends && props.profile.friends.length) {
+            firstInfo = `Became friends with ${props.profile.friends[props.profile.friends.length - 1]}`
+            firstInfoIcon = <Link />
+        } else if (props.profile.currLocation) {
+            firstInfo =
+                <span className={classes.InfoEntry}>{'Lives in '} <b>{props.profile.currLocation.name}</b></span>
+            firstInfoIcon = <Home/>
+        } else if (props.profile.education && props.profile.education.length) {
+                firstInfo = <span className={classes.InfoEntry}>{`Studie${props.profile.education[0].graduated ? 'd' : 's'} at `} <b>{props.profile.education[0].school}</b></span>
+                firstInfoIcon = <GradCap />
+        } else if (props.profile.hometown) {
+            firstInfo = <span className={classes.InfoEntry}>{'Comes from '} <b>{props.profile.hometown.name}</b></span>
+            firstInfoIcon = <Pin />
+        }
     }
 
-    let secondInfo;
 
 
     return (
@@ -45,7 +76,8 @@ const profileSummaryDropdown = (props) => {
 
 const mapStateToProps = state => {
     return {
-        authToken: state.auth.token
+        authToken: state.auth.token,
+        profile: state.users.singleProfile
     }
 }
 
