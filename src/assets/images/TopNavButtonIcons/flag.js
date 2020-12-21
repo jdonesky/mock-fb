6455fca08860ba66a562af5bd67e1d4f2117b658
Jon <jdonesky@gmail.com>
2020-12-21 07:44:04 -0500

    continued added styling to page/group create preview

--- src/assets/images/TopNavButtonIcons/flag.js
+++ src/assets/images/TopNavButtonIcons/flag.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const flag = () => (
+const flag = (props) => (
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
  width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000"
  preserveAspectRatio="xMidYMid meet">
-<metadata>
-Created by potrace 1.16, written by Peter Selinger 2001-2019
-</metadata>
+
 <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-fill="#000000" stroke="none">
+fill={props.fill || "#000000"} stroke="none">
 <path d="M1125 5091 c-205 -97 -247 -356 -81 -509 25 -23 62 -50 81 -59 l35
 -17 0 -2231 0 -2232 23 -21 c34 -32 96 -30 128 4 l24 26 3 1109 2 1109 1394 2
 1394 3 26 24 c42 40 36 91 -17 149 -479 520 -817 896 -815 903 2 5 181 202
--- src/components/Create/PagePreview/PagePreview.js
+++ src/components/Create/PagePreview/PagePreview.js
@@ -4,18 +4,50 @@ import React from 'react';
 import classes from './PagePreview.css';
 
 import Desktop from '../../../assets/images/MiscIcons/desktop';
+import getWindowDimensions from "../../../hooks/getWindowDimensions";
+import CreatePageCover from '../../../assets/images/Raster/createPagePreview.png';
+import CreateGroupCover from '../../../assets/images/Raster/createGroupPreview.png';
+import Flag from '../../../assets/images/BookmarkIcons/flag';
+
+
 
 const pagePreview = props => {
+
+    const {width, height} = getWindowDimensions()
+
+    let header;
+    if (props.preview === 'PAGE') {
+        header = (
+            <div className={classes.PageHeaderPositioner}>
+                <div className={classes.CoverImage} style={{backgroundImage: `url(${CreatePageCover})`, height: `${width * 0.23}px`}} />
+                <div className={classes.PageProfileHeader}>
+                    <div className={classes.PageProfileCircleOutline}>
+                        <div className={classes.PageProfileCircle}><Flag first="rgba(0,0,0,0.1)" second="rgba(0,0,0,0.15)"/></div>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (props.preview === 'GROUP') {
+        // header = ()
+    }
+
     return (
         <div className={classes.PreviewContainer}>
             <div className={classes.Header}>
                 <h4>Desktop Preview</h4>
                 <div className={classes.DesktopIcon}>
-                    <Desktop />
+                    <Desktop fill="#0a74ff"/>
                 </div>
             </div>
             <div className={classes.PageDisplay}>
-                <div className={classes.CoverImage} style={{backgroundImage: props.coverImage ? `url(${props.coverImage})` : null}} />
+                <div className={classes.PageHeaderPositioner}>
+                    <div className={classes.CoverImage} style={{backgroundImage: props.coverImage ? `url(${props.coverImage})` : `url(${CreatePageCover})`, height: `${width * 0.23}px`}} />
+                        <div className={classes.PageProfileHeader}>
+                            <div className={classes.PageProfileCircleOutline}>
+                                <div className={classes.PageProfileCircle}><Flag fill='rgba(0,0,0,0.4)'/></div>
+                            </div>
+                        </div>
+                </div>
             </div>
         </div>
     )
