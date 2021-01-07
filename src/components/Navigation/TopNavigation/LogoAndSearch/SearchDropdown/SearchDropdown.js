9b1d66b93d588a297ababf1486c210869cb05d9e
Jon <jdonesky@gmail.com>
2021-01-06 22:37:31 -0500

    adding recent searches to general search component in top nav bar

--- src/components/Navigation/TopNavigation/LogoAndSearch/SearchDropdown/SearchDropdown.js
+++ src/components/Navigation/TopNavigation/LogoAndSearch/SearchDropdown/SearchDropdown.js
@@ -1,15 +1,23 @@
 
-import React, {useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {connect} from 'react-redux'
 import classes from './SearchDropdown.css'
 import * as actions from '../../../../../store/actions/index';
 import Searchbar from "../../../../Search/Searchbar";
-import BackArrow from '../../../../../assets/images/TopNavButtonIcons/backArrow';
 import InlineDots from '../../../../UI/Spinner/InlineDots';
+
+import BackArrow from '../../../../../assets/images/TopNavButtonIcons/backArrow';
+import Flag from '../../../../../assets/images/TopNavButtonIcons/flag';
+import Avatar from '../../../../../assets/images/BookmarkIcons/user';
+import Question from '../../../../../assets/images/MiscIcons/question';
+import Delete from '../../../../../assets/images/MessengerIcons/close';
+
 
 const searchDropdown = (props) => {
 
-    const {authToken, onFetchPersonalActivity, personalActivity, onSearchAll, searchingAll, results, searchHistory, close, onCreateActivityRecord, activityLogKey} = props;
+    const {authToken, onFetchPersonalActivity, personalActivity, onSearchAll, searchingAll, results, close, onCreateActivityRecord, activityLogKey} = props;
+
+    const [searches, setSearches] = useState(null);
 
     useEffect(() => {
         console.log('search result', results)
@@ -20,12 +28,11 @@ const searchDropdown = (props) => {
        onFetchPersonalActivity(authToken, activityLogKey, 'GENERAL_SEARCH')
     }, [])
 
-    let recentSearches;
-    if (searchHistory && searchHistory.length) {
-        recentSearches = searchHistory.sort()
-    } else {
-        recentSearches = <div className={classes.Placeholder}>No recent searches</div>
-    }
+    useEffect(() => {
+
+    }, [])
+
+
     const filterResults = useCallback(phrase => {
         onSearchAll(authToken, phrase)
         const newActivity = {
@@ -34,8 +41,61 @@ const searchDropdown = (props) => {
             sortDate: new Date().getTime(),
             text: phrase,
         }
+        setSearches(prevState => {
+            if (prevState !== null) {
+                return [...prevState, newActivity]
+            } else {
+                return [newActivity]
+            }
+        })
         onCreateActivityRecord(authToken, activityLogKey, newActivity, 'PERSONAL')
     }, [])
+
+    let recentSearches;
+    if (!searches) {
+        if (personalActivity && personalActivity.length) {
+            console.log('IN personalActivity.length block')
+            recentSearches = personalActivity.sort((a,b) => a.sortDate > b.sortDate ? a : b);
+        } else {
+            recentSearches = <div className={classes.Placeholder}>No recent searches</div>
+        }
+    } else if (searches && searches.length) {
+        if (personalActivity && personalActivity.length) {
+            recentSearches = [...searches, ...personalActivity].sort((a,b) => a.sortDate > b.sortDate ? a : b)
+        } else if (!personalActivity) {
+            recentSearches = searches
+        }
+    }
+
+    if (recentSearches && recentSearches.length) {
+        recentSearches = recentSearches.map(search => {
+            let icon;
+            if (search.subject === 'USER') {
+                icon = <Avatar fill="white" />
+            } else if (search.subject === 'PAGE') {
+                icon = <Flag fill="white" />
+            } else {
+                icon = <Question fill="white" />
+            }
+
+            return (
+                <div className={classes.SearchRecord}>
+                    <div className={classes.SearchRecordLeftBlock}>
+                        <div className={classes.ProfileImage} style={{backgroundImage: search.image ? `url(${search.image})` : null }}>
+                            {search.image ? null : icon}
+                        </div>
+                        <div className={classes.SearchRecordText}>search.text</div>
+                    </div>
+                    <div className={classes.DeleteButtonContainer}>
+                        <div className={classes.DeleteButton}>
+                            <Delete fill="rgba(0,0,0,0.5)"/>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+
+    }
 
     return (
         <div className={classes.DropdownContainer}>
@@ -56,7 +116,6 @@ const mapStateToProps = state => {
     return {
         authToken: state.auth.token,
         results: state.search.results,
-        searchHistory: state.activity.searchHistory,
         searchingAll: state.search.searchingAll,
         activityLogKey: state.profile.activityLogKey,
         personalActivity: state.activity.personalActivity
